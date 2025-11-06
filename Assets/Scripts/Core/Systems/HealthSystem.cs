6f6725b13922416e73eab8030e944e8c63bdc547
Twaish <axiomeverything@gmail.com>
2025-11-06 14:41:54 +0100

    Add HealthSystem

---
+++ Assets/Scripts/Core/Systems/HealthSystem.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+    [SerializeField]
+    private int health = 100;
+
+    [SerializeField]
+    private int maxHealth = 100;
+
+    [SerializeField]
+    private int minHealth = 0;
+
+    public event Action<int> OnHealthChanged;
+
+    public void Damage(int damage)
+    {
+        health -= damage;
+        health = Mathf.Clamp(health, minHealth, maxHealth);
+        OnHealthChanged?.Invoke(health);
+    }
+
+    public int GetHealth()
+    {
+        return health;
+    }   
+}
