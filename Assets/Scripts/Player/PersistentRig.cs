b26c3969e1dd91b700f44d3f65f77413ccb7a030
Twaish <axiomeverything@gmail.com>
2025-11-24 13:59:37 +0100

    Remove newer persistent rigs on scene load

--- Assets/Scripts/Player/PersistentRig.cs
+++ Assets/Scripts/Player/PersistentRig.cs
@@ -2,8 +2,29 @@ using UnityEngine;
 
 public class PersistentRig : MonoBehaviour
 {
+    public float TimeOfCreation;
     void Awake()
     {
+        TimeOfCreation = Time.time;
+        GameObject[] rigs = GameObject.FindGameObjectsWithTag("Player");
+        foreach (var rig in rigs)
+        {
+            if (rig == this.gameObject) continue;
+
+            if (!rig.TryGetComponent(out PersistentRig other))
+            {
+                continue;
+            }
+
+            if (other.TimeOfCreation < TimeOfCreation)
+            {
+                Destroy(gameObject);
+            } else if (other.TimeOfCreation > TimeOfCreation)
+            {
+                Destroy(rig);
+            }
+        }
+
         DontDestroyOnLoad(gameObject);
     }
 }
